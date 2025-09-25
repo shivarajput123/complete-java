f16b20e0f8c68ca83b7dceca98de8f7c436e851d
shivarajput123 <chauhanshiva9839@gmail.com>
2025-09-25 20:29:35 +0530

    initial commit

---
+++ Challenge3.java
@@ -0,0 +1,35 @@
+// class patterns {
+//     public static void main(String[] args) {
+//          System.out.println("*");
+//          System.out.println("* *");
+//          System.out.println("* * *");
+//          System.out.println("* * * *");
+//          System.out.println("* * * * *");
+
+
+//     }
+// }
+
+// class patterns {
+//     public static void main(String[] args) {
+//          System.out.println("* * * * *");
+//          System.out.println("* * * *");
+//          System.out.println("* * *");
+//          System.out.println("* * ");
+//          System.out.println("* ");
+
+
+//     }
+// }
+
+class Challenge3 {
+    public static void main(String[] args) {
+         System.out.println("        *");
+         System.out.println("      * *");
+         System.out.println("    * * *");
+         System.out.println("  * * * *");
+         System.out.println("* * * * *");
+
+
+    }
+} 
---
+++ Challenge5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Challenge5 {
+    public static void main(String[] args) {
+         System.err.println("please enter your name :");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+
+     
+        System.out.println("welcome " + name + " coding");
+    }
+}
