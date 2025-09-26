6d2278e2b0b2a85f3b49c7b9927e749dd9018820
shivarajput123 <chauhanshiva9839@gmail.com>
2025-09-26 19:26:26 +0530

    new code

---
+++ Challenge-100/Challenge7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Challenge7{
+    public static void main(String[] args) {
+        System.out.println("please enter the  number:");
+        Scanner input = new Scanner (System.in);
+        System.out.print("firstValue:");
+        int a = input.nextInt();
+        System.out.print("secondValue:");
+        int b = input.nextInt();
+        b = a + b;
+        a = b - a;
+        b = b - a;
+        System.out.println("after swapping");
+        System.out.println("firstValue:" + a );
+        System.out.println("secondValue:" + b );
+
+
+    }
+}
