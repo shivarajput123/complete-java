dc15c9d61b465cab560d38123952a00a0474e757
shivarajput123 <chauhanshiva9839@gmail.com>
2025-09-26 19:45:45 +0530

    initial

--- Challenge-100/Challenge1.java
+++ Challenge-100/Challenge1.java
@@ -1,3 +1,5 @@
+//  1.Create a class to output “good morning” using a text 
+// editor and check output.
 public class Challenge1 {
     public static void main(String[] args) {
         System.out.println("goodmoring");
