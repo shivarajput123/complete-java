645828bf6741f3b896bd4aca42bc2ec07db44b06
shivarajput123 <chauhanshiva9839@gmail.com>
2025-09-26 19:36:29 +0530

    new code

---
+++ Challenge-100/Challenge9.java
@@ -0,0 +1,12 @@
+//  9. Create a program to calculate product of two floating points numbers
+public class Challenge9 {
+    public static void main(String[] args) {
+        
+    float firstNumber = 4.5f;
+    float secondNumber = 8.34f;
+    float multiply = firstNumber * secondNumber ;
+    System.out.println(multiply);
+     
+
+    }
+}
