8981b0d6f8adb2edbce533144488ccf7ed4c4422
shivarajput123 <chauhanshiva9839@gmail.com>
2025-09-26 19:52:37 +0530

    initial

--- Challenge-100/Challenge4.java
+++ Challenge-100/Challenge4.java
@@ -1,3 +1,6 @@
+// 4.Show the following patterns using single print 
+// statement
+
 public class Challenge4 {
     public static void main(String[] args) {
         System.out.println("*\n* *\n* * *\n* * * *\n* * * * *\n");
